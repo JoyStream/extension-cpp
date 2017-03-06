4b64f48d419889f0c6a6f77813d72f6b76a988a5
Bedeho Mender <bedeho.mender@gmail.com>
2017-03-06 08:03:42 +0100

    wip2

--- test/test_integrations.cpp
+++ test/test_integrations.cpp
@@ -1,63 +1,49 @@
+//
+// Created by Bedeho Mender on 28/02/17.
+//
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include "PollableInterface.hpp"
 #include "TorrentClientSwarm.hpp"
-#include <protocol_wire/protocol_wire.hpp>
 
-
-#include <boost/asio/impl/src.hpp>
+//#include <boost/asio/impl/src.hpp>
 
 #define POLLING_COUNT 3
 #define POLLING_SLEEP_DURATION 1*std::chrono_literals::s
 
-libtorrent::session * basic_session();
-libtorrent::torrent_info load(const std::string file);
+// Generate torrent content (not random)
+std::shared_ptr<libtorrent::torrent_info> create_torrent(std::ostream* file = 0,
+                                                         char const* name = "temporary",
+                                                         int piece_size = 16 * 1024,
+                                                         int num_pieces = 13,
+                                                         bool add_tracker = true,
+                                                         std::string ssl_certificate = "") {
 
-//
-// how to locate valid path to torrent file, and partial downloads,
-// which live in source tree?
-// how to cleanup files when done?
+}
 
 TEST(IntegrationTesting, Connectivity) {
 
 }
 
-/**
-// generate torrent content (not random)
-EXPORT std::shared_ptr<libtorrent::torrent_info> create_torrent(std::ostream* file = 0
-	, char const* name = "temporary", int piece_size = 16 * 1024, int num_pieces = 13
-	, bool add_tracker = true, std::string ssl_certificate = "");
-*/
-
 TEST(IntegrationTesting, OneToOne) {
 
-    std::string base_folder; // = <current folder of this binary>,
-    std::string data_source_folder; // = <folder where we are dumping stuff>
+    std::string base_folder; // = boost::this_folder(); <current folder of this binary>,
+    std::string data_source_folder; // = base_folder + boost::file_sep ? "xxx"
 
     // Create torrent file and content
-    libtorrent::torrent_info & ti; // create_torrent_file("base_folder_here")
-
-    // neutral
-    uint16_t number_of_normal_seeder_clients = 0,
-            number_of_normal_leecher_clients = 0,
-            number_of_observer_clients = 0;
-
-    // sellers
-    std::vector<protocol_wire::SellerTerms> seller_client_terms;
-
-    // buyers
-    std::vector<protocol_wire::BuyerTerms> buyer_client_terms;
+    //std::ostream* file
+    std::shared_ptr<libtorrent::torrent_info> ti = create_torrent_file("base_folder_here");
 
     // Create swarm
     TorrentClientSwarm swarm(base_folder,
                              data_source_folder,
                              ti,
-                             number_of_normal_seeder_clients,
-                             number_of_normal_leecher_clients,
-                             number_of_observer_clients,
-                             seller_client_terms,
-                             buyer_client_terms);
+                             1,
+                             1,
+                             0,
+                             std::vector<protocol_wire::SellerTerms>(),
+                             std::vector<protocol_wire::BuyerTerms>());
 
 
     // Run swarm event loop
