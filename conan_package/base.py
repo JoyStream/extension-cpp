6f1d9bb521e8bea2dd3097880a4e10c53cea6ef8
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2017-08-16 15:03:53 +0300

    add conan_package

---
+++ conan_package/base.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class ExtensionBase(ConanFile):
+    name = "Extension"
+    version = "0.1.1"
+    license = "(c) JoyStream Inc. 2016-2017"
+    url = "https://github.com/JoyStream/extension-conan.git"
+    git_repo = "git@github.com:JoyStream/extension-cpp.git"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    requires = "ProtocolSession/0.1.1@joystream/stable", "Libtorrent/1.1.1@joystream/stable"
+    build_policy = "missing"
+
+    def source(self):
+        raise Error("")
+
+    def configure(self):
+        self.options["Libtorrent"].deprecated_functions=False
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake repo/sources %s' % (cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src="repo/sources/include/")
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["extension"]
+
+        if str(self.settings.compiler) != "Visual Studio":
+            self.cpp_info.cppflags.append("-std=c++11")
---
+++ conan_package/test_package/example.cpp
@@ -0,0 +1,7 @@
+#include <extension/extension.hpp>
+#include <libtorrent/sha1_hash.hpp>
+
+int main() {
+    libtorrent::sha1_hash hash;
+    return 0;
+}
